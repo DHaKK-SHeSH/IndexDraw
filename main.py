f234de53ef95c0e0d14c27d3386adf008370df0c
unknown <dhakshesh.sam@gmail.com>
2025-03-17 20:48:29 +0100

    1703 Transparent Webcam Overlay

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from src import pptxTopng
 import os
 import cv2
 import numpy as np
-#
-# ppt_file = r"C:\Users\Dhakshesh\Downloads\SmartLiving.pptx"
+
+ppt_file = r"C:\Users\Dhakshesh\Downloads\SmartLiving.pptx"
 pptLocation = r"C:\Users\Dhakshesh\Desktop\Daily Contributions\IndexDraw\presentation/"
-#
-# pptImages = os.listdir(pptLocation)
-#
+
+pptImages = os.listdir(pptLocation)
+
 # if pptImages:
 #     for i in pptImages:
 #         os.remove(os.path.join(pptLocation, i))
@@ -17,7 +17,7 @@ pptLocation = r"C:\Users\Dhakshesh\Desktop\Daily Contributions\IndexDraw\present
 # print("Conversion complete!")
 
 
-width,height = int(1024*0.7),int(768*0.5)
+width,height = [2560 ,1440]
 
 
 cap = cv2.VideoCapture(0)
@@ -27,40 +27,35 @@ cap.set(4,height)
 pptImages = os.listdir(pptLocation)
 
 #custom lambda method to sort files
-sorted_pptImages = sorted(pptImages, key=lambda x: int(x.replace(".png","").split("_")[1]))
+sorted_pptImages = sorted(pptImages, key=lambda x: int(x.replace(".jpg","").split("_")[1]))
 
 #define start slide
-startSlide = 5
+startSlide = 23
+hgt, wdt = int(1440*0.2),int(2560*0.2)
 
-#setting window size
-#want the image to be inside a 700x700 box
-desiredSize = (700,700)
-#we now create the background for our slides to fit the new window size
-canvas = np.zeros((desiredSize[1],desiredSize[0],3),dtype=np.uint8)
+# trying to create a transparent webcam feed so the slides are unaffected
+alpha = 0.5 #provides 30% opacity to webcam
+beta = 1-alpha
+
 
 while True:
     success,img = cap.read()
     pathFullImage = os.path.join(pptLocation,sorted_pptImages[startSlide])
     currentSlide = cv2.imread(pathFullImage)
-    # currentSlide = cv2.resize(currentSlide,(width,height))  #resizing jus the ppt
-    #get the og dimensions of the slide
-    h,w,_ = currentSlide.shape
-
-    #resize image without affecting aspect ratio by calculating new proportions for width and height
-    scale = min(desiredSize[0]/w,desiredSize[1]/h)
-    new_w,new_h = int(w*scale), int(h*scale)
-    currentSlide = cv2.resize(currentSlide, (new_w, new_h))
-
-    #calculate padding and set the image in the center
-    x_offset = (desiredSize[0]-new_w)//2
-    y_offset = (desiredSize[1]-new_h)//2
-    #
-    canvas[y_offset:y_offset+new_h,x_offset:x_offset+new_w] = currentSlide
 
     cv2.imshow("Image",img)
-    #cv2.imshow("Slide",currentSlide)
-    #using canvas
-    cv2.imshow("Slide", canvas)
+    # overlay area
+    webcamImg = cv2.resize(img,(wdt,hgt))
+    h,w,_ = currentSlide.shape
+    # region of interest for opacity
+    roi = currentSlide[h-hgt:h,w-wdt:w]
+    # blend the images
+    blended = cv2.addWeighted(webcamImg,alpha,roi,beta,0)
+    currentSlide[h-hgt:h,w-wdt:w] = blended
+
+    cv2.namedWindow("Slide", cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty("Slide", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.imshow("Slide", currentSlide)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
--- src/pptxTopng.py
+++ src/pptxTopng.py
@@ -5,7 +5,7 @@ def convert_ppt_to_jpg(srcFile,tgtLocation):
     try:
         Presentation = Application.Presentations.Open(srcFile)
         for i in range(Presentation.Slides.Count):
-            Presentation.Slides[i].Export(tgtLocation +"slide_"+str(i)+".png", "PNG")
+            Presentation.Slides[i].Export(tgtLocation +"slide_"+str(i)+".jpg", "PNG")
         Presentation.Close()
     finally:
         Application.Quit()
